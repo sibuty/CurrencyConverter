9698972fc08962acbf6e028163b7b42e4563a596
Igor Goryainov <igor.goryainov@altarix.ru>
2018-02-02 03:34:42 +0400

    RxChains, handle ui for input fields and spinners

--- app/src/main/java/org/seriouslycompany/cc/main/MainActivity.kt
+++ app/src/main/java/org/seriouslycompany/cc/main/MainActivity.kt
@@ -7,19 +7,25 @@
 package org.seriouslycompany.cc.main
 
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.AppCompatSpinner
 import android.widget.EditText
+import com.jakewharton.rxbinding2.support.v4.widget.refreshes
+import com.jakewharton.rxbinding2.support.v4.widget.refreshing
+import com.jakewharton.rxbinding2.widget.itemSelections
 import com.jakewharton.rxbinding2.widget.textChanges
-import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import org.seriouslycompany.cc.R
-import org.seriouslycompany.cc.base.ext.ioMainTransformer
+import org.seriouslycompany.cc.base.ext.ioTransformer
+import org.seriouslycompany.cc.base.ext.mainTransformer
 import org.seriouslycompany.cc.base.ext.view
 import org.seriouslycompany.cc.di.ext.appComponent
+import org.seriouslycompany.cc.di.ext.bind
+import org.seriouslycompany.cc.di.ext.roundBeforeThree
+import org.seriouslycompany.cc.di.ext.toBigDecimalSafe
 import org.seriouslycompany.cc.main.currency.adapter.CurrencyArrayAdapter
 import java.math.BigDecimal
-import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,69 +34,151 @@ class MainActivity : AppCompatActivity() {
   private val etSecond by view<EditText>(R.id.et_second_currency)
   private val sFirst by view<AppCompatSpinner>(R.id.s_first_currency)
   private val sSecond by view<AppCompatSpinner>(R.id.s_second_currency)
+  private val srlContent by view<SwipeRefreshLayout>(R.id.srl_content)
 
-  private val viewState = ViewState();
+  private val clearOnDestroy = CompositeDisposable()
+  private val viewState = ViewState()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     appComponent.currencyRepository.currencyList()
+        .mainTransformer()
+        .doOnNext { srlContent.refreshing().accept(false) }
+        .ioTransformer()
+        .filter { it.isNotEmpty() }
         .map { it.map(Currency::getInstance) }
-        .map { CurrencyArrayAdapter(MainActivity@this, it) }
-        .observeOn(AndroidSchedulers.mainThread())
-        .ioMainTransformer()
+        .map { CurrencyArrayAdapter(MainActivity@ this, it) }
+        .mainTransformer()
         .doOnNext(sFirst::setAdapter)
         .doOnNext(sSecond::setAdapter)
         .subscribe()
+        .bind(clearOnDestroy)
+
+    srlContent.refreshes()
+        .doOnNext { appComponent.currencyRepository.refresh() }
+        .subscribe()
+        .bind(clearOnDestroy)
+
+    sFirst.itemSelections()
+        .skipInitialValue()
+        .map { sFirst.adapter.getItem(it) as Currency }
+        .map(Currency::getCurrencyCode)
+        .filter { viewState.sourceIsoCode != it }
+        .doOnNext { viewState.sourceIsoCode = it }
+        .filter { viewState.destinationIsoCode.isNotBlank() }
+        .concatMap { appComponent.ratesProvider.rate(viewState.sourceIsoCode, viewState.destinationIsoCode).toObservable() }
+        .filter { viewState.sourceConversionValue != it }
+        .doOnNext { viewState.sourceConversionValue = it }
+        .map { viewState.sourceValue.toPlainString() }
+        .doOnNext { viewState.clearSourceValue() }
+        .mainTransformer()
+        .subscribe(etFirst::setText)
+        .bind(clearOnDestroy)
+
+    sSecond.itemSelections()
+        .skipInitialValue()
+        .map { sSecond.adapter.getItem(it) as Currency }
+        .map(Currency::getCurrencyCode)
+        .filter { viewState.destinationIsoCode != it }
+        .doOnNext { viewState.destinationIsoCode = it }
+        .filter { viewState.sourceIsoCode.isNotBlank() }
+        .concatMap { appComponent.ratesProvider.rate(viewState.destinationIsoCode, viewState.sourceIsoCode).toObservable() }
+        .filter { viewState.destinationConversionValue != it }
+        .doOnNext { viewState.destinationConversionValue = it }
+        .concatMap { appComponent.ratesProvider.rate(viewState.sourceIsoCode, viewState.destinationIsoCode).toObservable() }
+        .filter { viewState.sourceConversionValue != it }
+        .doOnNext { viewState.sourceConversionValue = it }
+        .map { viewState.sourceValue.toPlainString() }
+        .doOnNext { viewState.clearSourceValue() }
+        .mainTransformer()
+        .subscribe(etFirst::setText)
+        .bind(clearOnDestroy)
 
     etFirst.textChanges()
         .map(CharSequence::toString)
-        .map(String::toBigDecimal)
+        .map(String::toBigDecimalSafe)
         .filter { viewState.sourceValue != it }
         .doOnNext { viewState.sourceValue = it }
-        .map { it.multiply(BigDecimal(2)) }
+        .map { it.multiply(BigDecimal(viewState.sourceConversionValue)).roundBeforeThree() }
         .filter { viewState.destinationValue != it }
         .doOnNext { viewState.destinationValue = it }
         .map { viewState.destinationValue.toPlainString() }
-        .observeOn(AndroidSchedulers.mainThread())
+        .mainTransformer()
         .subscribe(etSecond::setText)
+        .bind(clearOnDestroy)
 
     etSecond.textChanges()
         .map(CharSequence::toString)
-        .map(String::toBigDecimal)
+        .map(String::toBigDecimalSafe)
         .filter { viewState.destinationValue != it }
         .doOnNext { viewState.destinationValue = it }
-        .map { it.divide(BigDecimal(2)) }
+        .map { it.multiply(BigDecimal(viewState.destinationConversionValue)).roundBeforeThree() }
         .filter { viewState.sourceValue != it }
         .doOnNext { viewState.sourceValue = it }
         .map { viewState.sourceValue.toPlainString() }
-        .observeOn(AndroidSchedulers.mainThread())
+        .mainTransformer()
         .subscribe(etFirst::setText)
+        .bind(clearOnDestroy)
+
+    /*prepareInputField(etFirst,
+        etSecond,
+        { viewState.sourceValue },
+        { viewState.sourceValue = it },
+        { viewState.destinationValue },
+        { viewState.destinationValue = it },
+        { viewState.sourceConversionValue }
+    ).bind(clearOnDestroy)
+
+    prepareInputField(etSecond,
+        etFirst,
+        { viewState.destinationValue },
+        { viewState.destinationValue = it },
+        { viewState.sourceValue },
+        { viewState.sourceValue = it },
+        { viewState.destinationConversionValue }
+    ).bind(clearOnDestroy)*/
   }
 
-  override fun onStart() {
-    super.onStart()
+  override fun onDestroy() {
+    super.onDestroy()
+    clearOnDestroy.clear()
   }
+
+  /*private fun prepareInputField(sourceEtField: EditText,
+                                destinationEtField: EditText,
+                                sourceValueGetter: () -> BigDecimal,
+                                sourceValueSetter: (BigDecimal) -> Unit,
+                                destinationValueGetter: () -> BigDecimal,
+                                destinationValueSetter: (BigDecimal) -> Unit,
+                                conversionValue: () -> Double) =
+      sourceEtField.textChanges()
+          .map(CharSequence::toString)
+          .map(String::toBigDecimalSafe)
+          .filter { sourceValueGetter() != it }
+          .doOnNext(sourceValueSetter)
+          .map { it.multiply(BigDecimal(conversionValue())).roundBeforeThree() }
+          .filter { destinationValueGetter() != it }
+          .doOnNext(destinationValueSetter)
+          .map { destinationValueGetter().toPlainString() }
+          .mainTransformer()
+          .subscribe(destinationEtField::setText)*/
 
   class ViewState {
-    var sourceValue: BigDecimal = BigDecimal(0)
-    var destinationValue: BigDecimal = BigDecimal(0)
-  }
-}
+    var sourceValue: BigDecimal = BigDecimal(0).roundBeforeThree()
+    var destinationValue: BigDecimal = BigDecimal(0).roundBeforeThree()
+    var sourceIsoCode: String = ""
+    var destinationIsoCode: String = ""
+    var sourceConversionValue: Double = 1.0
+    var destinationConversionValue: Double = 1.0
 
+    fun clearSourceValue() {
+      sourceValue = BigDecimal(0).roundBeforeThree()
+    }
 
-fun String.toBigDecimal(): BigDecimal {
-  try {
-    return (DecimalFormat.getInstance() as DecimalFormat).apply {
-      isParseBigDecimal = true
-
-      decimalFormatSymbols = DecimalFormatSymbols().apply {
-        groupingSeparator = ','
-        decimalSeparator = '.'
-      }
-    }.parse(this) as BigDecimal
-  } catch (e: Exception) {
-    return BigDecimal(0)
+    fun clearDestinationValueValue() {
+      destinationValue = BigDecimal(0).roundBeforeThree()
+    }
   }
 }
